0fe652c52bdf504cc83351f19bf968f22ca5a36d
naturaltm <naturaltm@gmail.com>
2012-08-04 14:25:32 +0300

    Update battleMine.php

--- battleMine.php
+++ battleMine.php
@@ -9,10 +9,8 @@ class mineClass{
         $cUrlClass->goToPage('/mine/start/');
         $html->load($cUrlClass->goToPage("/mine/"));
         
-        $findTime = $html->find('script');             
-        $blabla = preg_replace("/[^0-9]/", "", $findTime[5]);
              
-        return $blabla;
+        return tru;
     }
 
 
