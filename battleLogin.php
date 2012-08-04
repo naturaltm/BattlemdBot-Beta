cb8d155636b53cfb3037ee2d75d3bfc8d3904a9c
naturaltm <naturaltm@gmail.com>
2012-08-04 19:43:31 +0300

    Update battleLogin.php

--- battleLogin.php
+++ battleLogin.php
@@ -7,17 +7,20 @@ class loginClass{
  
       function loginUser(){
       global $cUrlClass;
+      
       $postDataFormat = "player={$this->userName}&pass={$this->userPass}";
       $resultLogin = $cUrlClass->goToPage('/tlogin.php',$postDataFormat);
+      
       return $resultLogin;
       }
 
       function isLoged(){
       global $cUrlClass, $html;
+      
       $html->load($cUrlClass->goToPage('/player/'));      
       $result = $html->find('div[id=rpage] font[size=3] b');
-      if(!empty($result[0])){return true;}else{return false;}
       
+      if(!empty($result[0])){return true;}else{return false;}      
       }
 }
 
